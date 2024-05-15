9c8bef132057e90b33554aa1336ffec21f865c5a
Isitha Tennakoon <82481046+IsithaT@users.noreply.github.com>
2024-05-14 21:16:58 -0700

    flask

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,template_folder='templates', static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('primary.html')
+@app.route('/goals')
+def goals():
+    return render_template('goals.html')
+@app.route('/history')
+def history():
+    return render_template('history.html')
